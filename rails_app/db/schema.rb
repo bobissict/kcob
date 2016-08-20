b6f8aecedaf44eac46e2be9ec71d2feb6df74180
Clive Corbishley <corbishley.clive@gmail.com>
2016-08-20 12:18:16 +0200

    Adding house to profiles

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808105017) do
+ActiveRecord::Schema.define(version: 20160819220402) do
 
   create_table "industries", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160808105017) do
     t.boolean  "activated",         default: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.string   "house"
   end
 
   create_table "users", force: :cascade do |t|
